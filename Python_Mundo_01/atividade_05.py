620fd7c75853e69421ca0debd27e3be25b5e9d80
Franklin Alves <franklin_kaio@hotmail.com>
2022-11-05 17:19:03 -0300

    Antecessor e Sucessor.

---
+++ Python_Mundo_01/atividade_05.py
@@ -0,0 +1,5 @@
+#Exercício Python 5: 
+#Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+n = int(input('Digite um número: '))
+print('O número antecessor do numero digitado é {}\nE o sucessor é {}'.format(n-1, n+1))
