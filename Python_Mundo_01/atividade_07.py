2630cbb193f89cabb95949b7019a73910268bea6
Franklin Alves <franklin_kaio@hotmail.com>
2022-11-05 18:34:08 -0300

    Média Aritmética.

---
+++ Python_Mundo_01/atividade_07.py
@@ -0,0 +1,6 @@
+#Exercício Python 7: 
+#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+
+n1 = float(input('Digite a sua primeira nota(n1): '))
+n2 = float(input('Digite a sua segunda nota(n2): '))
+print('A sua nota média é = {:.2f}'.format((n1+n2)/2))
