f1123a57a53d53bd8cd50197e744baf1816c9ec7
Franklin Alves <franklin_kaio@hotmail.com>
2022-11-05 19:44:40 -0300

    Tabuada(*)

---
+++ Python_Mundo_01/atividade_09.py
@@ -0,0 +1,16 @@
+#Exercício Python 9:
+#Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada de multiplicação.
+
+n = int(input('Digite o número para ver sua tabuada de multiplicação: '))
+print('-'*13)
+print(n, 'x 1 = {}'.format(1*n))
+print(n, 'x 2 = {}'.format(2*n))
+print(n, 'x 3 = {}'.format(3*n))
+print(n, 'x 4 = {}'.format(4*n))
+print(n, 'x 5 = {}'.format(5*n))
+print(n, 'x 6 = {}'.format(6*n))
+print(n, 'x 7 = {}'.format(7*n))
+print(n, 'x 8 = {}'.format(8*n))
+print(n, 'x 9 = {}'.format(9*n))
+print(n, 'x 10 = {}'.format(10*n))
+print('-'*13)
