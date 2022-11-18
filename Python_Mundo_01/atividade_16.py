ec9a0682139e1d328b5410c1b917750da362a3a3
Franklin Alves <franklin_kaio@hotmail.com>
2022-11-17 23:50:18 -0300

    Quebrando um número.

---
+++ Python_Mundo_01/atividade_16.py
@@ -0,0 +1,5 @@
+# Exercício Python 16:
+# Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
+
+numero = float(input('Digite um numero real: '))
+print('O número digitado foi {} e a sua porção inteira é = {}.'.format(numero, int(numero)))
